de417f8bff31df88ad6fac52386c84bc4ed7cd39
Sy Le <lenguyensy@gmail.com>
2023-09-26 15:38:41 -0700

    now use redirect uri from origin

--- app/routes/api.auth.login.ts
+++ app/routes/api.auth.login.ts
@@ -1,15 +1,31 @@
 import { json, redirect, LoaderFunction } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import {
-  REDIRECT_PATH,
+  BASE_API_HOST,
+  LOGIN_CALLBACK_URL,
   SCOPE,
   confidentialClientApplication,
 } from "~/utils/backend/SSO";
 
-export async function loader() {
+export async function loader(args: LoaderArgs) {
+  const { request } = args;
+
+  let redirectUri = "";
+  if (BASE_API_HOST) {
+    redirectUri = BASE_API_HOST;
+  } else {
+    try {
+      redirectUri = `https://${new URL(request.url).hostname}`;
+    } catch (err) {}
+  }
+
+  redirectUri = `${redirectUri}${LOGIN_CALLBACK_URL}`;
+
   try {
     const loginUrl = await confidentialClientApplication.getAuthCodeUrl({
       scopes: SCOPE,
-      redirectUri: REDIRECT_PATH,
+      redirectUri,
+      state: redirectUri,
     });
     return redirect(loginUrl);
   } catch (err) {
